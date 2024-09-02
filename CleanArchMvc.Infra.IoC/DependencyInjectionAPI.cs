5af93fa6a239bb10c97f2a0dcd9cef5be8400d38
ALCM-bit <falcm-bit@outlook.com>
2024-09-02 20:59:33 -0300

    Add Web API project
    Starting development of Web API project

---
+++ CleanArchMvc.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,53 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Account;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using CleanArchMvc.Infra.Data.Identity;
+using CleanArchMvc.Infra.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.IoC;
+
+public static class DependencyInjectionAPI
+{
+    public static IServiceCollection AddInfrastructureAPI(this IServiceCollection serviceCollection,
+        IConfiguration configuration)
+    {
+        serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+        serviceCollection.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        serviceCollection.AddScoped<IAuthenticate, AuthenticateService>();
+
+        serviceCollection.AddAutoMapper(typeof(DomainToDtoMappingProfile));
+        serviceCollection.AddAutoMapper(typeof(DTOToCommandMappingProfile));
+
+        serviceCollection.AddScoped<ICategoryRepository, CategoryRepository>();
+        serviceCollection.AddScoped<IProductRepository, ProductRepository>();
+
+        serviceCollection.AddScoped<IProductService, ProductService>();
+        serviceCollection.AddScoped<ICategoryService, CategoryService>();
+
+        var myHandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
+        serviceCollection.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(myHandlers));
+
+
+
+        return serviceCollection;
+
+    }
+}
